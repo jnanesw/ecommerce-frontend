3883b4c2853b9a218c4b250a75a6a3566a047ca3
jnanesw <nanimca000@gmail.com>
2025-10-28 09:16:15 +0530

    Setup actions and reducer for Redux-Store

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,34 +1,22 @@
-import "./Product.css";
+import "./Products.css";
 import ProductCard from "./ProductCard";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchProducts } from "../store/actions";
 
 function Products() {
   const isLoading = false;
   const errorMessage = "";
 
-  const products = [
-    {
-      productId: 652,
-      productName: "Iphone Xs max",
-      image: "https://placehold.co/600x400",
-      description:
-        "Experience the latest in mobile technology with advanced cameras, powerful processing, and an all-day battery.",
-      quantity: 2,
-      price: 1450.0,
-      discount: 10.0,
-      specialPrice: 1305.0,
-    },
-    {
-      productId: 654,
-      productName: "MacBook Air M2s",
-      image: "https://placehold.co/600x400",
-      description:
-        "Ultra-thin laptop with Apple's M2 chip, providing fast performance in a lightweight, portable design.",
-      quantity: 0,
-      price: 2550.0,
-      discount: 20.0,
-      specialPrice: 2040.0,
-    },
-  ];
+  const products = useSelector(
+    (state) => state.products
+  )
+
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    dispatch(fetchProducts())
+  }, [dispatch])
 
   return (
     <div className="product-list">
---
+++ src/store/reducer/ProductReducer.js
@@ -0,0 +1,26 @@
+const initialState = {
+    products: null,
+    categories: null,
+    pagination: {}
+}
+
+export const productReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "FETCH_PRODUCTS":
+            return{
+                ...state,
+                products: action.payload,
+                pagination: {
+                    ...state.pagination,
+                    pageSize: data.pageSize,
+                    pageNumber: data.pageNumber,
+                    totalElements: data.totalElements,
+                    totalPages: data.totalPages,
+                    lastPage: data.lastPage,
+                }
+            }
+    
+        default:
+            state;
+    }
+}
